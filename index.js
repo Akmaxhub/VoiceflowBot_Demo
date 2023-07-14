1094bc773e7a0d86c950206cd48e6aa875721a4a
Ankur <Ankur13kr@gmail.com>
2023-07-14 11:12:20 -0400

    Added script for index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const script = document.createElement('script');
+    
+    script.src = 'index.js';
+    
+    // Append the <script> element to the <head> of the HTML document
+    document.head.appendChild(script);
+  });
