de355212b222c293205ce2fcdf5153d485ce06cf
Ankur <Ankur13kr@gmail.com>
2023-07-14 11:17:05 -0400

    added some changes for deployment

--- index.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    const script = document.createElement('script');
-    
-    script.src = 'index.js';
-    
-    // Append the <script> element to the <head> of the HTML document
-    document.head.appendChild(script);
-  });
